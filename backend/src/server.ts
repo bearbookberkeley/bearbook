01f91b67dde9317a9159d3ac9a4b9fa6cce284da
maxmwang <orangemax888@gmail.com>
2023-03-09 15:29:27 -0800

    Folder structure

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+import express from 'express';
+import http from 'http';
+
+dotenv.config();
+
+const PORT = Number(process.env.PORT) || 5000;
+
+const app = express();
+const server = http.createServer(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve('client/dist')));
+}
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
